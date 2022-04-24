9d6235bfac9fd1992efacc86cdf6d2b95d8fe4d8
soyElSando <german.tito.sandoval@gmail.com>
2022-04-24 13:07:25 -0300

    componente de lista de items

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Task} from '../../Task'
 import { TASK } from 'src/app/mock-tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -10,6 +11,7 @@ import { TASK } from 'src/app/mock-tasks';
 export class TaskItemComponent implements OnInit {
 
   @Input() task: Task= TASK[0]
+  faTimes=faTimes;
 
   constructor() { }
 
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Task} from "../../Task"
 import {TASK} from "../../mock-tasks"
 
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -10,6 +11,7 @@ import {TASK} from "../../mock-tasks"
 export class TasksComponent implements OnInit {
 
   tasks: Task[]=TASK;
+  
 
   constructor() { }
 
