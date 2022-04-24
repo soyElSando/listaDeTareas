3a5266baadb502e8289a111a140e9637da41d7de
soyElSando <german.tito.sandoval@gmail.com>
2022-04-24 11:57:52 -0300

    primer commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import {Task} from "./Task"
+export const TASK: Task[]=[
+    {
+        id: 1,
+        text: "terminar modulo de Angular",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: "Agosto 5 a las 17:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Boostrap",
+        day: "Agosto 5 a las 12:00",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Leer mi libro favorito",
+        day: "Agosto 7 a las 12:00",
+        reminder: false
+    }
+]
