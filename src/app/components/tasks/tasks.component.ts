d207bdb9cd82fd55c46eccf658fc5ac2eaa76168
soyElSando <german.tito.sandoval@gmail.com>
2022-04-24 17:25:21 -0300

    base de datos OK

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/service/task.service';
 import {Task} from "../../Task"
-import {TASK} from "../../mock-tasks"
-
 
 @Component({
   selector: 'app-tasks',
@@ -10,12 +9,17 @@ import {TASK} from "../../mock-tasks"
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[]=TASK;
+  tasks: Task[]=[];
   
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+  ) {}
 
   ngOnInit(): void {
+     this.taskService.getTasks().subscribe((tasks)=>{
+      this.tasks = tasks
+     });
   }
 
 }
